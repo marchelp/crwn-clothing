842a54984c21c20066d4e129972c5a10051c4ab6
marchelp <marchel.adias@gmail.com>
2025-08-15 18:23:15 +0700

    added out sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,43 @@
+import Directory from "../../components/directory/directory.component";
+
+const Home = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl:
+        "https://www.quiksilver.com/cdn/shop/articles/how-to-wash-a-hat_750x.jpg?v=1742233159",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFu_zWnXn7hssSWHXjRzEhKeRkcLguL1K4iA&s",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl:
+        "https://v2brand.it/cdn/shop/collections/IMG_COLLECTION.jpg?v=1739525037",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl:
+        "https://thumbs.dreamstime.com/b/blond-woman-against-autumn-nature-landscape-36634739.jpg",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl:
+        "https://www.gazman.com.au/cdn/shop/articles/BestWinterJackets_Blog_Winter_FeatureTile_1016x711_751ce837-eb2d-4f0b-9096-8741517400be.jpg?v=1719462605",
+    },
+  ];
+
+  return (
+    <Directory categories={categories} />
+  );
+}
+
+export default Home;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,65 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8-1tyMLHgG-KAows36D3wWpDneYfV89g",
+  authDomain: "crwn-clothing-db-f0586.firebaseapp.com",
+  projectId: "crwn-clothing-db-f0586",
+  storageBucket: "crwn-clothing-db-f0586.firebasestorage.app",
+  messagingSenderId: "485709028750",
+  appId: "1:485709028750:web:84e6d086637d4ebbd498df"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  // if user data does not exist
+  // create / set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+
+    // if user data exist
+    // return userDocRef
+
+    return userDocRef;
+  }
+}
