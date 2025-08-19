7fdea71c23295d176316a2188c2370e77e188ba4
marchelp <marchel.adias@gmail.com>
2025-08-19 21:23:45 +0700

    created authentication page alongsid sign in forms

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -12,7 +12,7 @@ const BUTTON_TYPES_CLASSES = {
 }
 
 const Button = ({ children, buttonType, ...otherProps }) => {
-  return <button className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`}>{children}</button>;
+  return <button className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`} {...otherProps}>{children}</button>;
 };
 
 export default Button;
